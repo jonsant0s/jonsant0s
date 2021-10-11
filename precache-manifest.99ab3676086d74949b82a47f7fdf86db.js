362c8d13977bcade10b05ed5c890f3faac366b38
Jonathan <jonsant0s@outlook.com>
2021-10-11 12:28:52 -0600

    Updates

---
+++ precache-manifest.99ab3676086d74949b82a47f7fdf86db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19b6e0bd45ba5e93c4fb955d26c9b163",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1af9375e83e5bf4d03e1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dfead429822b8c66a379",
+    "url": "/static/js/2.708f0d96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.708f0d96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1af9375e83e5bf4d03e1",
+    "url": "/static/js/main.084270cb.chunk.js"
+  },
+  {
+    "revision": "927bafc0a9b222dbc7c4",
+    "url": "/static/js/runtime-main.c871e8c2.js"
+  }
+]);
